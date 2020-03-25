eea7d938eafa09d8764ea0acb0f0d69adb82e572
Pauli Jokela <pauli.jokela@didstopia.com>
2020-03-25 14:48:12 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-FILTER="[{\"filtertype\":\"age\",\"source\":\"creation_date\",\"direction\":\"older\",\"unit\":\"days\",\"unit_count\":${CURATOR_RETENTION_DAYS:-7}},{\"filtertype\":\"pattern\",\"kind\":\"prefix\",\"value\":\"${CURATOR_INDEX_PATTERN:-logstash-*}\"}]"
+FILTER="[{\"filtertype\":\"age\",\"source\":\"creation_date\",\"direction\":\"older\",\"unit\":\"days\",\"unit_count\":${CURATOR_RETENTION_DAYS:-7}},{\"filtertype\":\"pattern\",\"kind\":\"prefix\",\"value\":\"${CURATOR_INDEX_PATTERN}\"}]"
 while true; do
-  curator_cli --host elasticsearch delete_indices --filter_list ${FILTER}
+  curator_cli --host ${CURATOR_ES_HOST} delete_indices --filter_list ${FILTER}
   set -e
-  sleep ${CURATOR_SLEEP_SECS:-86400}
+  sleep ${CURATOR_SLEEP_SECS}
   set +e;
 done
