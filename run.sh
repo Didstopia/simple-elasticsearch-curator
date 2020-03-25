36a0ebb0f85ccfe2b98807e0979a7ba391f111ed
Pauli Jokela <pauli.jokela@didstopia.com>
2020-03-25 15:00:46 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FILTER="[{\"filtertype\":\"age\",\"source\":\"creation_date\",\"direction\":\"older\",\"unit\":\"days\",\"unit_count\":${CURATOR_RETENTION_DAYS:-7}},{\"filtertype\":\"pattern\",\"kind\":\"prefix\",\"value\":\"${CURATOR_INDEX_PATTERN}\"}]"
+FILTER="[{\"filtertype\":\"age\",\"source\":\"creation_date\",\"direction\":\"older\",\"unit\":\"${CURATOR_RETENTION_UNIT}\",\"unit_count\":${CURATOR_RETENTION_AMOUNT}},{\"filtertype\":\"pattern\",\"kind\":\"prefix\",\"value\":\"${CURATOR_INDEX_PATTERN}\"}]"
 while true; do
   curator_cli --host ${CURATOR_ES_HOST} delete_indices --filter_list ${FILTER}
   set -e
